d8b471883820fa71653560b65a0d9966a166b36b
Julien Rémond <jremond@ideal-groupe.com>
2019-07-05 12:00:55 +0200

    initialized project, created empty grid

---
+++ 2048/resources/js/components/Home.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-container>
+        <v-layout column>
+            <v-flex>
+                <h1>2048</h1>
+            </v-flex>
+            <v-flex>
+                <v-layout v-for="(row, index) in grid" row :key="index" >
+                    <v-flex v-for="(tile, index) in row" :key="index">{{ tile }}</v-flex>
+                </v-layout>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                grid: [
+                    [0, 0, 0, 0],
+                    [0, 0, 0, 0],
+                    [0, 0, 0, 0],
+                    [0, 0, 0, 0],
+                ],
+                rowValues: []
+            }
+        },
+        created () {
+            this.grid.forEach(row => {
+                if (true) {
+                    
+                }
+            });
+        },
+        methods: {
+
+        }
+    }
+</script>
